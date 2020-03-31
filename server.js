0638342805dce2eb0565bc15bc5ce3dc13319148
Your Name <angelicrf@gmail.com>
2020-03-31 13:53:53 -0700

    WServer-json

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const jsonServer = require('json-server');
 
 const port = process.env.PORT || 5000;
+app.use('/api', jsonServer.defaults(),jsonServer.router('db.json'));
 app.use(express.json({extended: false}));
 /*app.use(express.static(__dirname));*/
 app.use(express.static('build'));
